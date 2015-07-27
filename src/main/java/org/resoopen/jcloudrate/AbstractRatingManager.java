5a56d6358c206885307d5ea719ec62d7819612bb
yaohongkok <yaohong.kok@gmail.com>
2015-07-26 21:18:24 -0400

    First upload to j-cloudRate

---
+++ src/main/java/org/resoopen/jcloudrate/AbstractRatingManager.java
@@ -0,0 +1,13 @@
+package org.resoopen.jcloudrate;
+
+/**
+ * Hello world!
+ *
+ */
+public class AbstractRatingManager 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
